c9a4fff32476a910ff3644cb9e495ce645836d59
Rindel Subaan <valsubaan@yahoo.com>
2022-03-26 17:48:28 +0800

    navbar change

--- app/src/main/java/com/example/mobileappprc/Home.java
+++ app/src/main/java/com/example/mobileappprc/Home.java
@@ -19,7 +19,6 @@ public class Home extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
