6322ab7fd4bfe8e270c667b775e82f6622387470
Rindel Subaan <valsubaan@yahoo.com>
2022-03-26 12:35:15 +0800

    navbar change

--- app/src/main/java/com/example/mobileappprc/Home.java
+++ app/src/main/java/com/example/mobileappprc/Home.java
@@ -1,18 +1,37 @@
 package com.example.mobileappprc;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 public class Home extends AppCompatActivity {
 
-    Button startBtn;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        startBtn = (Button) findViewById(R.id.startBtn);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
