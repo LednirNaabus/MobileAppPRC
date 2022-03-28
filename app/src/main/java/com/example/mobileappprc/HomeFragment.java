54899b8b948350c45057c3cc3b7e12f2f365038a
Rindel Subaan <valsubaan@yahoo.com>
2022-03-28 19:48:34 +0800

    HomeFragment class

--- app/src/main/java/com/example/mobileappprc/HomeFragment.java
+++ app/src/main/java/com/example/mobileappprc/HomeFragment.java
@@ -4,15 +4,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    NestedScrollView nestedScrollView;
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    ArrayList<MainData> dataArrayList = new ArrayList<MainData>();
+    MainAdapter adapter;
+    int page = 1, limit = 10;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        nestedScrollView = findViewById(R.id.scroll_view);
     }
 }
