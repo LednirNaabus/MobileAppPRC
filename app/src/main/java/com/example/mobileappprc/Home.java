7984ced439e5587585557519e69bf403ddc12661
Rindel Subaan <valsubaan@yahoo.com>
2022-03-28 16:06:08 +0800

    remove button

--- app/src/main/java/com/example/mobileappprc/Home.java
+++ app/src/main/java/com/example/mobileappprc/Home.java
@@ -9,6 +9,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
