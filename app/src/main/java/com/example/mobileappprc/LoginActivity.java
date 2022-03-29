bb641ec113c4cd54defcd37aee08126736fa1800
Rindel Subaan <valsubaan@yahoo.com>
2022-03-29 15:23:09 +0800

    Login activity change

--- app/src/main/java/com/example/mobileappprc/LoginActivity.java
+++ app/src/main/java/com/example/mobileappprc/LoginActivity.java
@@ -2,18 +2,31 @@ package com.example.mobileappprc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomeActivity();
+            }
+        });
+
     }
 
-    public void signIn(View view) {
-        setContentView(R.layout.activity_home);
+    private void openHomeActivity() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
     }
+
 }
