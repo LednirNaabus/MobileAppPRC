53e657ed18f1ff41ca1fe37ba25bdbf3662fca88
Rindel Subaan <valsubaan@yahoo.com>
2022-03-29 22:40:37 +0800

    status page add

---
+++ app/src/main/java/com/example/mobileappprc/StatusFragment.java
@@ -0,0 +1,20 @@
+package com.example.mobileappprc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class StatusFragment extends Fragment {
+    @NonNull
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_status, container, false);
+    }
+}
